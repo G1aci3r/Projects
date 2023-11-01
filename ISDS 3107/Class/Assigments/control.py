04fc39b15ef684f2cb8cd7df1f61d19e819eab1b
G1aci3r <149526575+G1aci3r@users.noreply.github.com>
2023-11-01 16:17:11 -0500

    Delete ISDS 3107 directory

--- ISDS 3107/Class/Assigments/control.py
+++
@@ -1,8 +0,0 @@
-today = "Thursday"
-
-if today == "Tuesday":
-  print("We get to write code!")
-elif today == 'Thursday':
-    print ('Yeah, we write code on Thursdays also.')
-else:
-  print("Time to learn business law!")
--- ISDS 3107/Class/Classwork/four_loops.py
+++
@@ -1,8 +0,0 @@
-l = [5, 6, 10, 20]
-
-sum = 0
-for num in l:
-    sum += num
-
-avg = sum / len(l)
-print(avg)
